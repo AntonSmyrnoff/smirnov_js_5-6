b2fed94e1951585de0ecacf746b0e27c9472d2fb
AntonSmyrnoff <a.smyrnoff@gmail.com>
2016-11-23 17:57:45 +0200

    second

--- js/script.js
+++ js/script.js
@@ -1,39 +1,54 @@
+		
 var ms = 1;
-
-setInterval(function() {
-	if (ms%100 < 10) {
-	milisec.innerHTML = "0" + ms%100;
-	} else {
-		milisec.innerHTML = ms%100;
-		}
-	ms++;
-}, 10);
-
-
-
 var s = 1;
-
-setInterval(function() {
-	if (s%60 < 10) {
-	sec.innerHTML = "0" + s%60;
-	} else {
-		sec.innerHTML = s%60;
-		} 
-	s++;
-}, 1000);
-
-
-
 var m = 1;
 
-setInterval(function() {
-	if (m%60 < 10) {
-	min.innerHTML = "0" + m%60;
-} else {
-	min.innerHTML = m%60;
+function timer() {
+	
+	var timerMilisec = setInterval(function() {
+		if (ms%100 < 10) {
+		milisec.innerHTML = "0" + ms%100;
+		} else {
+			milisec.innerHTML = ms%100;
+			}
+		ms++;
+	}, 10);
+
+	var timerSec = setInterval(function() {
+		if (s%60 < 10) {
+		sec.innerHTML = "0" + s%60;
+		} else {
+			sec.innerHTML = s%60;
+			} 
+		s++;
+	}, 1000);
+
+	var timerMin = setInterval(function() {
+		if (m%60 < 10) {
+		min.innerHTML = "0" + m%60;
+	} else {
+		min.innerHTML = m%60;
+	}
+		m++;
+	}, 60000);
+
+	start_button.innerHTML = 'PAUSE';
+	start_button.style.backgroundColor = 'blue';
+	start_button.addEventListener( 'click', cont );
+};
+
+function cont() {
+	clearInterval(timerMilisec);
+	clearInterval(timerSec);
+	clearInterval(timerMin);
 }
-	m++;
-}, 60000);
+
+
+start_button.addEventListener( 'click', timer );
+
+
+
+
 
 
 
